71da3b2f4ad8bec62e6475fe1e83eea3b846d0d3
dmostowfi <deanna.mostowfi@kellogg.northwestern.edu>
2022-05-20 19:15:21 +0000

    authenticated complete

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+    def new
+    end 
+
+    #this is how we authenticate
+    def create
+        #step 1: check to see if there's a user with the email we entered
+        @user = User.find_by({"email"=>params["email"]})
+        if @user #checks if user is in database
+        #step 1a: if yes, check password (go to step 2)
+        #step 2: check password to see if it matches user's password
+            if params["password"] == @user["password"] #params["password"] is the password that was typed in 
+            #step 2a: if yes, go to companies page 
+                redirect_to "/companies"
+            else
+            #step 2b: if no, go back to login page
+                redirect_to "/sessions/new"
+            end
+        else
+        #step 1b: if no, go back to login page  
+            redirect_to "/sessions/new"
+        end
+    end
+end
