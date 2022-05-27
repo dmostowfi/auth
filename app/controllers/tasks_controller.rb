a2b34eb2cf8825f24ee22094030965972dc1de54
dmostowfi <deanna.mostowfi@kellogg.northwestern.edu>
2022-05-27 15:30:51 +0000

    todo labs

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,23 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.where({"user_id" => @current_user["id"]) #As a signed-in user, I want to see only my tasks
     @task = Task.new
   end
+
 
   def create
     @task = Task.new
     @task["description"] = params["task"]["description"]
+    @task["user_id"] = @current_user["id"] #As a signed in user, I want to create new tasks for myself
     @task.save
     redirect_to "/tasks"
   end
 
   def destroy
     @task = Task.find_by({ "id" => params["id"] })
-    @task.destroy
+    if @task["user_id"] == @current_user
+      @task.destroy
+    end
     redirect_to "/tasks"
   end
 end
